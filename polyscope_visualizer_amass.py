8b58036b4b3c8b44b26cf9491df4344d63fdcad0
kulendu <kulendu>
2024-06-17 11:49:47 +0530

    Add: Trimesh visualizer for saving the frames and rendering the video

--- polyscope_visualizer_amass.py
+++ polyscope_visualizer_amass.py
@@ -5,12 +5,15 @@ import os
 import tqdm
 import cv2
 import trimesh
+import pyrender
 
 import polyscope as ps 
 from smplx import SMPL
 import numpy as np
 import torch
 import trimesh
+import imageio
+from PIL import Image
 
 import argparse
 
@@ -40,7 +43,7 @@ ps.set_front_dir("y_front")
 ps.set_navigation_style("free")
 
 # Ensure the output directory exists
-output_dir = 'frames'
+output_dir = 'trimesh_frames'
 if not os.path.exists(output_dir):
     os.makedirs(output_dir)
 
@@ -128,23 +131,23 @@ class Retargetter:
             vertices = output.vertices[0].detach().numpy()
 
             print(f"Global Orientation shape : {global_orient.shape} and length: {len(global_orient)}")
-            # mesh = trimesh.Trimesh(vertices, faces)
+            main_mesh = trimesh.Trimesh(vertices, faces)
 
 
-            ps.register_surface_mesh("AMASS_SMPL", vertices, faces)
+            # ps.register_surface_mesh("AMASS_SMPL", vertices, faces)
 
             # camera_position = [2.0, 2.0, 2.0]  # Change this to your desired camera position
             # look_at_position = [0.0, 0.0, 0.0]  # Change this to your desired target position
             # up_direction = [1.0, 1.0, 0.0]  # Change this to your desired up direction
             
-            ps.look_at((5., 0., 7.), (-0.5, 1., 1.))
+            # ps.look_at((5., 0., 7.), (-0.5, 1., 1.))
 
             # rabit_data = './rabit_data/shape/mean.obj'
 
-            # mesh = trimesh.load_mesh(rabit_data)
+            # mesh = trimesh.load_mesh(main_mesh)
             # vertices = mesh.vertices
             # faces = mesh.faces
-            # ps_mesh = ps.register_surface_mesh("RaBit", vertices, faces)
+            ps_mesh = ps.register_surface_mesh("RaBit", vertices, faces)
 
             # ps.show()
 
@@ -158,9 +161,9 @@ class Retargetter:
             # ps.clear()
             print(f"Frame: {frame_index}")
             # continue
-            # break
+            break
 
-    print("------------------- Successfully captured all the frames -------------------")
+    # print("------------------- Successfully captured all the frames -------------------")
 
 
 # loading the rabit model
@@ -199,8 +202,119 @@ def compile_frames_to_video(output_dir, video_filename, fps=60):
 
 
 
-# calling the main function
+
+class Visualizer:
+    def trimesh_visualizer():
+
+        amass_file = '/home/kulendu/SMPL-Manipulation/ACCAD/Male2MartialArtsExtended_c3d/Extended_1_stageii.npz'
+        # pose_file = './AMASS_pose.pkl'
+        # amass_file = path_to_amass
+
+        # Load the dataset
+        data = np.load(amass_file)
+        # data = joblib.load(amass_file)
+
+        print(f"Data keys for AMASS: {data.keys()}")
+
+        frame_count = data['poses'].shape[0]
+        # frame_index = 0
+        # poses = data['poses'][frame_index]
+        betas = data['betas'][:10]
+        # trans = data['trans'][frame_index]
+
+        print(f"Shape Coefficients: {len(betas)}")
+        # print(f"Poses: {len(poses)}")
+
+        smpl_model_path = '/home/kulendu/SMPL-Manipulation/SMPL_NEUTRAL.pkl'
+
+        # Load SMPL model
+        smpl = SMPL(model_path=smpl_model_path, gender='neutral', create_transl=False)
+
+        # poses = poses[:72]
+
+        '''24*3 = 72 joints : 24 joints and for each joints, 3 DoF
+           The first 3 values represents the joint rotation of the root joint (Global orientation)
+           The remaining 69 values represent the local rotations of the 23 joints  (Local Orientation) 
+        '''
+
+        # Ensure betas have length 10
+        if len(betas) != 10:
+            raise ValueError(f"Expected shape parameters (betas) of length 10, but got {len(betas)}")
+
+        # Convert pose and shape to tensors with float32 type
+        # unsqueeze(0) adds an extra dimension at the 0th position 
+
+
+        # body_pose = torch.tensor(poses[3:], dtype=torch.float32).unsqueeze(0)  # Exclude the global orientation
+        # global_orient = torch.tensor([0.0, 1.0, 0.0], dtype=torch.float32).unsqueeze(0) # [x,y,z] here the global rotation is dona only along y axis
+
+        # print(f"Original global orientation {global_orient}")
+
+        betas = torch.tensor(betas, dtype=torch.float32).unsqueeze(0)
+        # trans = torch.tensor(trans, dtype=torch.float32).unsqueeze(0)
+        faces = smpl.faces
+
+        frames = []
+        # loop for every frames
+        for frame_index in range(frame_count):
+            poses = data['poses'][frame_index]
+            # betas = data['betas'][:10]
+            trans = data['trans'][frame_index]
+
+            print(f"Shape Coefficients: {len(betas)}")
+            print(f"Poses: {len(poses)}")
+
+            poses = poses[:72]
+
+
+            body_pose = torch.tensor(poses[3:], dtype=torch.float32).unsqueeze(0)  # Exclude the global orientation
+            global_orient = torch.tensor([0.0, 1.0, 0.0], dtype=torch.float32).unsqueeze(0) # [x,y,z] here the global rotation is dona only along y axis
+
+            print(f"Original global orientation {global_orient}")
+
+
+            # Generate body mesh from pose and shape
+            output = smpl(global_orient=global_orient, body_pose=body_pose, betas=betas)
+            vertices = output.vertices[0].detach().numpy()
+
+            print(f"Global Orientation shape : {global_orient.shape} and length: {len(global_orient)}")
+            scene = trimesh.Scene()
+            mesh = trimesh.Trimesh(vertices, faces)
+
+
+            # rabit_data = './rabit_data/shape/mean.obj'
+
+            vertices = mesh.vertices
+            faces = mesh.faces
+            scene.add_geometry(mesh)
+            # mesh.show()
+            
+            frame_png = scene.save_image(resolution=[1920, 1080], visible=False)
+            image_array = np.frombuffer(frame_png, dtype=np.uint8)
+            image = cv2.imdecode(image_array, cv2.IMREAD_UNCHANGED)
+
+            # Save image using cv2
+            cv2.imwrite(os.path.join(output_dir, f'frame_{frame_index:04d}.png'), image)
+
+            # frames.append(imageio.imread(frame_png))
+
+
+
+
+            
+            print("Successfully called the visualizer")
+            
+            # making directories for storing the video
+            # filename_per_frame = os.path.join(output_dir, f'frame_{frame_index:04d}.png')
+            print(f"Frame: {frame_index}")
+
+    
+                    
+
 if __name__ == '__main__':
-    Retargetter.amass_visualizer()
-    compile_frames_to_video(output_dir, 'output_video_from_pkl.mp4', fps=60)
+    # Retargetter.amass_visualizer()
+    # compile_frames_to_video(output_dir, 'output_video_from_pkl.mp4', fps=60)
     # Retargetter.load_rabit()
+    # Pyrender_visualizer.visualizer()
+    Visualizer.trimesh_visualizer()
+    compile_frames_to_video(output_dir, 'output_video_from_trimesh.mp4', fps=60)
